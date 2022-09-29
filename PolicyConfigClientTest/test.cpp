7a318973f33bb9fdb064b443183d7a686e83d747
westelh <westelh@gmail.com>
2022-09-29 12:29:42 +0900

    Remove unused tests

--- PolicyConfigClientTest/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "PolicyConfigClient.h"
-
-TEST(TestSetDefaultAudioEndPoint, InvalidDeviceId) {
-	ASSERT_FALSE(SetDefaultAudioEndPoint(TEXT("SomeInvalidId"), ERole::eCommunications));
-	ASSERT_FALSE(SetDefaultAudioEndPoint(TEXT("SomeInvalidId"), ERole::eMultimedia));
-	ASSERT_FALSE(SetDefaultAudioEndPoint(TEXT("SomeInvalidId"), ERole::eConsole));
-}
