ce503609be4ff9143b0ffae6b6efccb12679cd6b
westelh <westelh@gmail.com>
2022-10-04 18:43:48 +0900

    Namespace change

--- HandyAudioControl/IPolicyConfig.h
+++ HandyAudioControl/IPolicyConfig.h
@@ -2,81 +2,84 @@
 #include <Mmreg.h>
 #include <Mmdeviceapi.h>
 
-interface DECLSPEC_UUID("f8679f50-850a-41cf-9c72-430f290290c8") IPolicyConfig;
-class DECLSPEC_UUID("870af99c-171d-4f9e-af0d-e63df40c2bc9") CPolicyConfigClient;
-// ----------------------------------------------------------------------------
-// Query interface:
-// CComPtr<IPolicyConfig> PolicyConfig;
-// PolicyConfig.CoCreateInstance(__uuidof(CPolicyConfigClient));
-// 
-// @compatible: Windows 7 and Later
-// ----------------------------------------------------------------------------
-interface IPolicyConfig : public IUnknown
-{
-public:
+namespace HandyAudioControl {
 
-    virtual HRESULT GetMixFormat(
-        PCWSTR,
-        WAVEFORMATEX**
-    );
+    interface DECLSPEC_UUID("f8679f50-850a-41cf-9c72-430f290290c8") IPolicyConfig;
+    class DECLSPEC_UUID("870af99c-171d-4f9e-af0d-e63df40c2bc9") CPolicyConfigClient;
+    // ----------------------------------------------------------------------------
+    // Query interface:
+    // CComPtr<IPolicyConfig> PolicyConfig;
+    // PolicyConfig.CoCreateInstance(__uuidof(CPolicyConfigClient));
+    // 
+    // @compatible: Windows 7 and Later
+    // ----------------------------------------------------------------------------
+    interface IPolicyConfig : public IUnknown
+    {
+    public:
 
-    virtual HRESULT STDMETHODCALLTYPE GetDeviceFormat(
-        PCWSTR,
-        INT,
-        WAVEFORMATEX**
-    );
+        virtual HRESULT GetMixFormat(
+            PCWSTR,
+            WAVEFORMATEX**
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE ResetDeviceFormat(
-        PCWSTR
-    );
+        virtual HRESULT STDMETHODCALLTYPE GetDeviceFormat(
+            PCWSTR,
+            INT,
+            WAVEFORMATEX**
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetDeviceFormat(
-        PCWSTR,
-        WAVEFORMATEX*,
-        WAVEFORMATEX*
-    );
+        virtual HRESULT STDMETHODCALLTYPE ResetDeviceFormat(
+            PCWSTR
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE GetProcessingPeriod(
-        PCWSTR,
-        INT,
-        PINT64,
-        PINT64
-    );
+        virtual HRESULT STDMETHODCALLTYPE SetDeviceFormat(
+            PCWSTR,
+            WAVEFORMATEX*,
+            WAVEFORMATEX*
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetProcessingPeriod(
-        PCWSTR,
-        PINT64
-    );
+        virtual HRESULT STDMETHODCALLTYPE GetProcessingPeriod(
+            PCWSTR,
+            INT,
+            PINT64,
+            PINT64
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE GetShareMode(
-        PCWSTR,
-        struct DeviceShareMode*
-    );
+        virtual HRESULT STDMETHODCALLTYPE SetProcessingPeriod(
+            PCWSTR,
+            PINT64
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetShareMode(
-        PCWSTR,
-        struct DeviceShareMode*
-    );
+        virtual HRESULT STDMETHODCALLTYPE GetShareMode(
+            PCWSTR,
+            struct DeviceShareMode*
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE GetPropertyValue(
-        PCWSTR,
-        const PROPERTYKEY&,
-        PROPVARIANT*
-    );
+        virtual HRESULT STDMETHODCALLTYPE SetShareMode(
+            PCWSTR,
+            struct DeviceShareMode*
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetPropertyValue(
-        PCWSTR,
-        const PROPERTYKEY&,
-        PROPVARIANT*
-    );
+        virtual HRESULT STDMETHODCALLTYPE GetPropertyValue(
+            PCWSTR,
+            const PROPERTYKEY&,
+            PROPVARIANT*
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetDefaultEndpoint(
-        __in PCWSTR wszDeviceId,
-        __in ERole eRole
-    );
+        virtual HRESULT STDMETHODCALLTYPE SetPropertyValue(
+            PCWSTR,
+            const PROPERTYKEY&,
+            PROPVARIANT*
+        );
 
-    virtual HRESULT STDMETHODCALLTYPE SetEndpointVisibility(
-        PCWSTR,
-        INT
-    );
-};
+        virtual HRESULT STDMETHODCALLTYPE SetDefaultEndpoint(
+            __in PCWSTR wszDeviceId,
+            __in ERole eRole
+        );
+
+        virtual HRESULT STDMETHODCALLTYPE SetEndpointVisibility(
+            PCWSTR,
+            INT
+        );
+    };
+}
