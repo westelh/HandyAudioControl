a58de7b097497328e2762c77d892357ebe69109d
westelh <westelh@gmail.com>
2022-10-04 18:47:34 +0900

    Hide some implementations

--- HandyAudioControl/MMDeviceClient.h
+++ HandyAudioControl/MMDeviceClient.h
@@ -5,14 +5,5 @@
 #include "Utils.h"
 
 namespace HandyAudioControl {
-
-	UniqueCOMPtr<IMMDeviceEnumerator> GetIMMDeviceEnumerator();
-
-	UniqueCOMPtr<IMMDeviceCollection> GetIMMDeviceCollection(UniqueCOMPtr<IMMDeviceEnumerator>& enumerator, EDataFlow dataFlow, DWORD dwStateMask);
-
-	UINT GetIMMDeviceCount(UniqueCOMPtr<IMMDeviceCollection>& collection);
-
-	UniqueCOMPtr<IMMDevice> GetIMMDevice(UniqueCOMPtr<IMMDeviceCollection>& collection, UINT i);
-
 	std::vector<UniqueCOMPtr<IMMDevice>> EnumerateAudioDevices(EDataFlow dataFlow, DWORD dwStateMask);
 }
