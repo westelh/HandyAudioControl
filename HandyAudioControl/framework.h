fe61f5a82057a53120defd74d7723c1a5777d125
westelh <westelh@gmail.com>
2022-10-12 09:15:45 +0900

    Remove macro name confliction

--- HandyAudioControl/framework.h
+++ HandyAudioControl/framework.h
@@ -12,6 +12,13 @@
 #include <mmdeviceapi.h>
 #include <Functiondiscoverykeys_devpkey.h>
 
+// Get rid of confliction for GetCurrentTime macro 
+#pragma push_macro("GetCurrentTime")
+#pragma push_macro("TRY")
+#undef GetCurrentTime
+#undef TRY
+
+// cpp winrt headers here
 #include <unknwn.h> // To enable support for non-WinRT interfaces, unknwn.h must be included before any C++/WinRT headers.
 #include <windows.ui.xaml.hosting.desktopwindowxamlsource.h>
 #include <winrt/Windows.UI.Xaml.h>
@@ -21,6 +28,10 @@
 #include <winrt/windows.ui.xaml.controls.primitives.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+
+#pragma pop_macro("TRY")
+#pragma pop_macro("GetCurrentTime")
+
 // C ランタイム ヘッダー ファイル
 #include <stdlib.h>
 #include <malloc.h>
